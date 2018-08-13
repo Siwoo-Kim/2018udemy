c2d30ddfeff0373a5d101ee50dc00b07985ca030
SiWoo Kim <35586824+Siwoo-Kim@users.noreply.github.com>
2018-08-12 18:47:07 -0700

    first commit

--- src/main/java/com/siwoo/nio/PathMain.java
+++ src/main/java/com/siwoo/nio/PathMain.java
@@ -2,8 +2,15 @@ package com.siwoo.nio;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author SiWoo Kim,
@@ -17,7 +24,36 @@ import java.nio.file.Path;
 public class PathMain {
 
     public static void main(String[] args) {
+        List<Path> paths = new ArrayList<>();
+        //FileSystems.getDefault().getPath() - Relative to Working Directory
+        Path path = FileSystems.getDefault().getPath("WorkingDirectory.txt");
+        paths.add(path);
+        //path = FileSystems.getDefault().getPath("C:\\Users\\Siwoo\\IdeaProjects\\_2018udemy\\files\\Subdirectory.txt");
+        //path = FileSystems.getDefault().getPath("C:", "Users", "Siwoo", "IdeaProjects", "_2018udemy", "files", "Subdirectory.txt");
+        //Path filePath = FileSystems.getDefault().getPath("files/Subdirectory.txt");
+        path = FileSystems.getDefault().getPath("files", "Subdirectory.txt");
+        path = Paths.get(".", "files", "Subdirectory.txt");
+        // C:\\Users\\Siwoo\IdeaProjects\\_2018udemy\\files\\Subdirectory.txt
 
+        paths.add(path);
+        path = FileSystems.getDefault().getPath("..","OutThere.txt");
+        paths.add(path);
+        path = Paths.get("C:\\Users\\Siwoo\\IdeaProjects\\OutThere.txt");
+        paths.add(path);
+        //Relative path is better practice.
+        // '.' refers to the working directory.
+        path = Paths.get(".");
+        System.out.println(path.toAbsolutePath());
+        path = Paths.get(System.getProperty("user.home"), "downloads");
+        Path samePath = Paths.get(System.getProperty("user.home"), ".", "dummy", "..", "downloads");
+        assertTrue(path.endsWith(samePath.normalize()));
+        System.out.println(samePath.normalize());
+
+        path = FileSystems.getDefault().getPath(".", "files", "..", "files", "Subdirectory.txt");
+        paths.add(path);
+
+        paths.forEach(_path ->  assertTrue(_path.toFile().exists()));
+        paths.forEach(PathMain::printFile);
     }
 
     private static void printFile(Path path) {
